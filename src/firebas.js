912f791b49ed98557018d7abed92a0dda3589326
jys <runnway@naver.com>
2020-09-16 00:14:09 +0900

    firebase setup

---
+++ src/firebas.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAaH0mbY0ni_cZOfO5xEzpv-RUEJ9Dr9bo",
+  authDomain: "rankers-b5c5f.firebaseapp.com",
+  databaseURL: "https://rankers-b5c5f.firebaseio.com",
+  projectId: "rankers-b5c5f",
+  storageBucket: "rankers-b5c5f.appspot.com",
+  messagingSenderId: "262336150767",
+  appId: "1:262336150767:web:88b97da4f699c912c2ef1a",
+  measurementId: "G-E4WCBBE8VM"
+};
+
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from './firebas';
 
 ReactDOM.render(
   <React.StrictMode>
