eda8cb24b123e6edcf26591d9c717111f5f0ce91
jys <runnway@naver.com>
2020-09-17 00:27:51 +0900

    Logout Complete

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,2 +1,16 @@
 import React from "react";
-export default () => <span>Profile</span>;
+import { AuthService } from "fbase";
+import { useHistory } from "react-router-dom";
+
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    AuthService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
